c495707a5a136dc841e0b620fdb5907c83d02ffd
Lungile Phakathi <lungiphakz@gmail.com>
2026-04-20 22:26:09 +0200

    admin login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+         User::create([
+            'name' => 'Bongiwe Phakathi',
+            'email' => 'lungiphakz12@gmail.com',
+            'password' => Hash::make('Bongi@1997'),
+            'role' => 'councilor', // if your system has roles
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
