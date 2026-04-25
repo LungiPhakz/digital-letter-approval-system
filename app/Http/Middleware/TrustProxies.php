3be828bf5ff37d1c821c0e386a2e63caf2a39235
Lungile Phakathi <lungiphakz@gmail.com>
2026-04-25 10:46:20 +0200

    Add Dockerfile for deployment

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -16,4 +16,6 @@ class TrustProxies extends Middleware
      * The headers that should be used to detect proxies.
      */
     protected $headers = \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL;
+
+    
 }
