1f89735702b5566411f4a17793a155bae96daf1f
Lungile Phakathi <lungiphakz@gmail.com>
2026-04-25 09:02:15 +0200

    correct a signature and stamp storage

--- resources/views/resident/login.blade.php
+++ resources/views/resident/login.blade.php
@@ -18,8 +18,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
 
 <!-- SDK -->
-<script src="/_sdk/element_sdk.js"></script>
-<script src="/_sdk/data_sdk.js"></script>
+
 
 <style>
 
